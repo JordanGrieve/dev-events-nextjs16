d4287b440b7a73fe60b3b38231ab95c3dc2fb555
Jordan Grieve <grievejordan@yahoo.com>
2025-10-29 20:50:15 +0000

    fix: correct API endpoint and improve text formatting in Page component

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,22 @@ import EventCard from '@/components/EventCard';
 import ExploreBtn from '@/components/ExploreBtn';
 
 const Page = async () => {
-  const respone = await fetch('/api/events/featured', { cache: 'no-store' });
-  const { data: events } = await respone.json();
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/events/featured`,
+    { cache: 'no-store' }
+  );
+
+  const { data: events } = await response.json();
+
   return (
     <section>
       <h1 className="text-center">
         The Hub For Every Dev <br /> Event You Cant Miss
       </h1>
       <p className="text-center m-5">
-        Hackathons, Meetups, Conferencess, All in One Place
+        Hackathons, Meetups, Conferences — All in One Place
       </p>
+
       <ExploreBtn />
 
       <div className="mt-20 space-y-7">
@@ -27,4 +33,5 @@ const Page = async () => {
     </section>
   );
 };
+
 export default Page;
